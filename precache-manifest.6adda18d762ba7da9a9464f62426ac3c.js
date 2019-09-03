f8d5d7281b254c4604f7324c03655c71ac1c00a8
Heri <tomislav.herout@gmail.com>
2019-09-03 22:21:24 +0200

    initial commit

---
+++ precache-manifest.6adda18d762ba7da9a9464f62426ac3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0df065d6c33175142bfff8aa95bb09a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5d10784c40f58dc0085",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3665775fe3d40da06f71",
+    "url": "/static/js/2.2cf0af37.chunk.js"
+  },
+  {
+    "revision": "d5d10784c40f58dc0085",
+    "url": "/static/js/main.333eaff8.chunk.js"
+  },
+  {
+    "revision": "5b3761eb7e101af9c753",
+    "url": "/static/js/runtime~main.213aca38.js"
+  }
+]);
